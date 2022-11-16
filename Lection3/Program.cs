985fb486ae7a9a1ff59387d12de3f354e79854da
Pavel <rut100500@yandex.ru>
2022-11-16 17:02:47 +0300

    Метод с текстом

---
+++ Lection3/Program.cs
@@ -0,0 +1,16 @@
+﻿//*void Method1(string msg)
+//{
+//    Console.WriteLine(msg);
+//}
+//Method1("Все бует хорошо");
+
+void Method2(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+}
+Method2("Ты молодец!!", 5);
