7939a8f4f73599b093ea9cc6e536fbb092c55611
Pavel <rut100500@yandex.ru>
2022-11-22 19:58:10 +0300

    Задать массив и найти индекс нужного числа

---
+++ task1/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = { 2, 4, 6, 7, 44, 33, 1, 22, 345, 33, 33, 111, 11};
+int n = array.Length;
+Console.WriteLine($"Длина масссива = {n}");
+int find = 22;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine($"Искомое число имеет индекс = {index}");
+        break;
+    }
+    index++;
+}
