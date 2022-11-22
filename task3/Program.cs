ca749affce522f211bbed28ed2d1d07b6b5c2130
Pavel <rut100500@yandex.ru>
2022-11-22 20:02:11 +0300

    Заменить символ в заданном текстовом массиве

---
+++ task3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Дан текст, в котором надо все пробелы заменить на "|"
+
+string text = "Он говорил на том изысканном французском языке,"
+ + "на котором не только говорили, но и думали наши деды, и с теми "
+ + "тихими, покровительственными интонациями, которые свойственны"
+ + "состаревшемуся в свете и при дворе значительному человеку.";
+
+ //  string s = "qwerty"
+ // s[3] это буква r
+
+ string Replace(string text, char oldValue, char newValue)
+ {
+    string result = String.Empty; // инициализация строковой переменной result
+    int length = text.Length; // показывает количество символов в стоке
+    for (int i = 0; i < length; i++)
+    {
+        if(text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+ }
+
+string newText = Replace(text, 'о', 'ы'); // кавычки ординарные, в них без лишних пробелов
+Console.WriteLine(newText);
