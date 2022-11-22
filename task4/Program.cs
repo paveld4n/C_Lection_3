8d8d806b24c133f0f42f73d19fc35c6756e9dc2c
Pavel <rut100500@yandex.ru>
2022-11-22 20:06:03 +0300

    отсортировать массив от мин к макс

---
+++ task4/Program.cs
@@ -0,0 +1,31 @@
+﻿// отсортировать массив от минимального к максимальному значению
+
+int[] arr = {2, 4, 11, 22, 3, 9, 4, 1, 2, 4};
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] < array[minPosition]) minPosition = j;
+        }
+        int temp = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temp;
+    }
+}
+PrintArray(arr);
+SelectSort(arr);
+PrintArray(arr);
